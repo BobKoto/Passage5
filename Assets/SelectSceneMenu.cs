931c448de1209a0433004312f729b8745d0dd21d
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-08-28 00:59:21 -0400

    Add Menu Script, canvas, and Scene

---
+++ Assets/SelectSceneMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SelectSceneMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void  OnButtonCapsulePlayer()
+    {
+        SceneManager.LoadScene("MainScene01"); 
+    }
+    public void OnButtonRobotAvatarPlayer()
+    {
+        SceneManager.LoadScene("MainScene02");
+    }
+    public void OnButtonExit()
+    {
+        Application.Quit();
+    }
+
+
+}
+
