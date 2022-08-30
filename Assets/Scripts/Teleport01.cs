8a8e32055ea474ad3c94db911e517c14ab785911
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-08-30 03:58:58 -0400

    Teleport development still inconsistent

--- Assets/Scripts/Teleport01.cs
+++ Assets/Scripts/Teleport01.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Teleport01 : MonoBehaviour
 {
-    public GameObject player;
-    public Transform playerTransform;
+    public GameObject player, playerCameraRoot;
+    public Transform playerTransform, playerCameraRootTransform;
     public Vector3 teleportToThisPosition;
     public Quaternion rotateToThisRotation;
+    public Vector3 rotateVector;
+    public bool teleportPerformed;
+
+   // IEnumerator WaitToMove;
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +29,24 @@ public class Teleport01 : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("teleport01 sees Triggercollision " + other.ToString());
-        playerTransform.SetPositionAndRotation(teleportToThisPosition, rotateToThisRotation);
+        Debug.Log("teleport01 sees TriggerENTER " + other.ToString());
+        if (!teleportPerformed)
+        {
+            playerTransform.SetPositionAndRotation(teleportToThisPosition, rotateToThisRotation);
+            StartCoroutine(WaitToMove());
+          //  teleportPerformed = true;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("teleport01 sees TriggerStay " + other.ToString());
+        if (!teleportPerformed)
+        {
+            playerTransform.SetPositionAndRotation(teleportToThisPosition, rotateToThisRotation);
+            StartCoroutine(WaitToMove());
+         //   teleportPerformed = true;
+        }
+
     }
     private void OnCollisionEnter(Collision other)
     {
@@ -37,5 +57,12 @@ public class Teleport01 : MonoBehaviour
     {
         Debug.Log("teleport01 sees collisionSTAY " + other.ToString());
         playerTransform.SetPositionAndRotation(teleportToThisPosition, rotateToThisRotation);
+    }
+    IEnumerator WaitToMove()
+    {
+        yield return new WaitForSeconds (5f);
+        Debug.Log("teleport01 supposedly SPINS THE CAM?");
+        playerCameraRootTransform.Rotate(rotateVector);
+
     }
 }
