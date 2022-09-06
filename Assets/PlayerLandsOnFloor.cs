06110cdcd6e235899c64d5458e727ae91a2d4ab8
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-09-05 23:28:45 -0400

    Start dev on script for landing on the center cube

---
+++ Assets/PlayerLandsOnFloor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLandsOnFloor : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HELLO FROM Player hit the floor ...");
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("cube hit by " + collision);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player hit the floor ...");
+        }
+    }
+}
