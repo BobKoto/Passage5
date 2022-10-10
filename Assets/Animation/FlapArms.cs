6b2b0e7034266c62fc34f76b4bfe20c70796f23a
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-10-10 01:28:05 -0400

    WIP Arm waving layer working but... we're changing

--- Assets/Animation/FlapArms.cs
+++ Assets/Animation/FlapArms.cs
@@ -4,38 +4,51 @@ using UnityEngine;
 
 public class FlapArms : StateMachineBehaviour
 {
-    Animator xanim;
-   // readonly int flapArms = 5;
-    
-    
-  //  OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+    // WARNING!! Parameter changes in this script(and likely others)
+    // WILL NOT APPEAR in EDITOR PLAYMODE unless the Animated GameObject is selected in the editor
+
+    //  OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        xanim = GameObject.Find("PlayerArmature").GetComponent<Animator>() ;
+      //  Animator xanim;
+      //  xanim = GameObject.Find("PlayerArmature").GetComponent<Animator>() ;
         int flapArmsHash = Animator.StringToHash(animator.parameters[5].name);
-        xanim.SetBool(flapArmsHash, true);
-        Debug.Log("ArmUp layer Entered animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
-        Debug.Log("params[5] = " + animator.parameters[5].name + " value = " + xanim.GetBool(flapArmsHash));
-            //+ animator.parameters[5].Equals(xanim.GetBool(animator.parameters[5].name)));//gobbledeegook that doesnt work
+        animator.SetBool(flapArmsHash, true);
+
+        var animClipsSize = animator.GetNextAnimatorClipInfo(1).Length;
+        if (animClipsSize > 0)
+        {
+          //   Debug.Log("Array size is " + animClipsSize + " Next clip is " + animator.GetNextAnimatorClipInfo(1)[0].clip);
+
+        }
+  
+      //  Debug.Log("ArmUp layer ENTERED animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
+        Debug.Log("ENTER params[5] = " + animator.parameters[5].name + " value = " + animator.GetBool(flapArmsHash));
+
     }
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       // Animator xanim;
+       //  xanim = GameObject.Find("PlayerArmature").GetComponent<Animator>();
+        int flapArmsHash = Animator.StringToHash(animator.parameters[5].name);
+        animator.SetBool(flapArmsHash, false);
+      //  Debug.Log("ArmUp layer EXITED animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
+        Debug.Log("EXIT params[5] = " + animator.parameters[5].name + " value = " + animator.GetBool(flapArmsHash));
+    }
+
+
+
+
+
+
+
 
     // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
     //    
     //}
-
-    // OnStateExit is called before OnStateExit is called on any state inside this state machine
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        xanim = GameObject.Find("PlayerArmature").GetComponent<Animator>();
-        int flapArmsHash = Animator.StringToHash(animator.parameters[5].name);
-        xanim.SetBool(flapArmsHash, false);
-        Debug.Log("ArmUp layer EXITED animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
-        Debug.Log("params[5] = " + animator.parameters[5].name + " value = " + xanim.GetBool(flapArmsHash));
-        //+ animator.parameters[5].Equals(xanim.GetBool(animator.parameters[5].name)));//gobbledeegook that doesnt work
-
-    }
 
     // OnStateMove is called before OnStateMove is called on any state inside this state machine
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
