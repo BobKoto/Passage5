d01cc6161cd884299680171b2980e89f0b5300cd
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-10-11 23:29:37 -0400

    WIP Finally got robot to Stop Waving while moving!
    State Behavior Script using animator.SetWeight(layer, integer value)

--- Assets/Animation/FlapArms.cs
+++ Assets/Animation/FlapArms.cs
@@ -10,31 +10,50 @@ public class FlapArms : StateMachineBehaviour
     //  OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-      //  Animator xanim;
-      //  xanim = GameObject.Find("PlayerArmature").GetComponent<Animator>() ;
-        int flapArmsHash = Animator.StringToHash(animator.parameters[5].name);
-        animator.SetBool(flapArmsHash, true);
+     //   int flapArmsHash = Animator.StringToHash(animator.parameters[5].name);
+        int baseLayerSpeedHash = Animator.StringToHash(animator.parameters[0].name);
 
-        var animClipsSize = animator.GetNextAnimatorClipInfo(1).Length;
-        if (animClipsSize > 0)
+      //  float flapArmsWeight;
+      //  bool flapArmsBool;
+        float speedBaseLayer;
+
+        speedBaseLayer = animator.GetFloat(baseLayerSpeedHash);
+     //   flapArmsBool = animator.GetBool(flapArmsHash);
+        if (speedBaseLayer > 1f)  //is character moving? and if so we don't want to flap arms 
         {
-          //   Debug.Log("Array size is " + animClipsSize + " Next clip is " + animator.GetNextAnimatorClipInfo(1)[0].clip);
-
+            animator.SetLayerWeight(1, 0);    //weight 0 stops arm flaps
         }
-  
-      //  Debug.Log("ArmUp layer ENTERED animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
-        Debug.Log("ENTER params[5] = " + animator.parameters[5].name + " value = " + animator.GetBool(flapArmsHash));
+        else
+            animator.SetLayerWeight(1, 1);     //weight 1 lets arms flap 
+        //var animClipsSize = animator.GetNextAnimatorClipInfo(1).Length;
+        //if (animClipsSize > 0)
+        //{
+
+        // Debug.Log("Array size is " + animClipsSize + " Next clip WEIGHT is " + animator.GetNextAnimatorClipInfo(1)[0].weight);
+        //animator.GetNextAnimatorClipInfo(1)[0].weight = 1; // can't do because ReadOnly
+
+        //animator.SetLayerWeight(1, 1);
+        //flapArmsWeight = animator.GetNextAnimatorClipInfo(1)[0].weight;
+        //flapArmsWeight = animator.GetLayerWeight(1);
+
+        // Debug.Log("Starting clip : " + animator.GetNextAnimatorClipInfo(1)[0].clip);
+        //flapArmsWeight = animator.GetLayerWeight(1);
+        //Debug.Log("flaparms clip  Weight = " + flapArmsWeight + " flapArmsBool is " + flapArmsBool);
+        //}
+
+        //  Debug.Log("ArmUp layer ENTERED animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
+        //  Debug.Log("ENTER params[5] = " + animator.parameters[5].name + " value = " + animator.GetBool(flapArmsHash));
+        //  Debug.Log(" flapArmsWeight = flapArms ['RobotArmsUp'].weight = " + flapArmsWeight);
+
 
     }
     // OnStateExit is called before OnStateExit is called on any state inside this state machine
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       // Animator xanim;
-       //  xanim = GameObject.Find("PlayerArmature").GetComponent<Animator>();
         int flapArmsHash = Animator.StringToHash(animator.parameters[5].name);
         animator.SetBool(flapArmsHash, false);
       //  Debug.Log("ArmUp layer EXITED animator= " + animator.name + "  stateInfoSpeed= " + stateInfo.speed + " layerIndex =" + layerIndex);
-        Debug.Log("EXIT params[5] = " + animator.parameters[5].name + " value = " + animator.GetBool(flapArmsHash));
+      //  Debug.Log("EXIT params[5] = " + animator.parameters[5].name + " value = " + animator.GetBool(flapArmsHash));
     }
 
 
