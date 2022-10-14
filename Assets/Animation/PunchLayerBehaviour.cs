08c3134756a22f131a62eec3aa999acc952739f4
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-10-14 03:42:09 -0400

    WIP Robot Punch/press Animation working
    Animation of robot pressing/punching (something) working ok. Some script creates, moves and deletes.

---
+++ Assets/Animation/PunchLayerBehaviour.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PunchLayerBehaviour : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Scene activeScene;
+        int punchHash = Animator.StringToHash(animator.parameters[6].name);
+        // waveArms = animator.GetBool(flapArmsHash);
+        activeScene = SceneManager.GetActiveScene();
+
+            if (activeScene.buildIndex == 2 )   //here we try to only do punches in sceneIndex 2 //the avatar scene 
+            {
+                int baseLayerSpeedHash = Animator.StringToHash(animator.parameters[0].name);  // get the speed name 
+                float speedBaseLayer;
+                speedBaseLayer = animator.GetFloat(baseLayerSpeedHash);  //get the speed 
+                if (speedBaseLayer > 1f)  //is character moving? and if so we don't want to punch
+                {
+                    animator.SetLayerWeight(2, 0);    //weight 0 inhibits punch
+                }
+                else
+                    animator.SetLayerWeight(2, 1);     //does a punch 
+            }
+            else animator.SetLayerWeight(2, 0);    //weight 0 stops arm flaps
+
+        //For this to  work you gotta have an EXIT state and a TRANSITION to it in layerIndex 2
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    Debug.Log(" Punch Exited ");
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/PlayerAnimButtons.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimButtons : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+    public void OnPressPressed()
+    {
+        anim.SetTrigger("Punch");
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
