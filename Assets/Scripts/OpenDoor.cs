12d7f964b5c42d58d1e95967144448252d4dfa1f
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-10-19 02:30:40 -0400

    WIP Robot press to open door (animation)
    NOT Working yet.

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+     Animator anim;
+    //public AudioSource audiosource;
+    //public AudioClip clip;
+    public AudioManager audioManager;
+    public Transform otherTransform;
+    public float pressableDistance = 3f;
+    bool tooFarToPress, closeEnoughToPress;
+    Material mat;
+    MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        audioManager = GameObject.Find("Audio Manager").GetComponent<AudioManager>();
+        mat = GetComponent<MeshRenderer>().material;
+        if (!otherTransform)
+        {
+            otherTransform = GameObject.Find("Right_Hand").GetComponent<Transform>();
+            // otherTransform = GameObject.Find("PlayerArmature").GetComponent<Transform>();
+            tooFarToPress = true; // IF and ONLY IF we start too far from a pressable object
+        }
+        //  Debug.Log("hello from " + this.name );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (otherTransform)
+        {
+            float dist = Vector3.Distance(otherTransform.position, transform.position);
+            if (dist <= pressableDistance)  //we are close enough 
+            {
+                if (!closeEnoughToPress && tooFarToPress)
+                {
+                    //  print("Distance to other just became Close : " + dist);
+                    closeEnoughToPress = !closeEnoughToPress;
+                    tooFarToPress = !tooFarToPress;
+                    mat.color = Color.red;
+                }
+            }
+            if (dist > pressableDistance)  //we are too far   AND always true on start 
+            {
+                if (closeEnoughToPress && !tooFarToPress)
+                {
+                    //  print("Distance to other just became too Far: " + dist);
+                    tooFarToPress = !tooFarToPress;
+                    closeEnoughToPress = !closeEnoughToPress;
+                    mat.color = Color.blue;
+                }
+            }
+        } // if other
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collided with " + collision.collider);
+        mat.color = Color.green;
+        audioManager.PlayAudio(audioManager.clipapert);
+        anim.SetTrigger("DoorSlideDown");
+    }
+}
