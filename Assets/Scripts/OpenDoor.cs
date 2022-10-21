ca8c54b54026dbd7cf1016b3498e87a325408d9a
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-10-21 02:52:36 -0400

    WIP Import unity TMPro essentials
    to try to fix vanishing text on some buttons. And we did some script work to add Unity Editor functionality to the OpenDoor script.

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class OpenDoor : MonoBehaviour
 {
      Animator anim;
@@ -9,10 +11,14 @@ public class OpenDoor : MonoBehaviour
     //public AudioClip clip;
     public AudioManager audioManager;
     public Transform otherTransform;
-    public float pressableDistance = 3f;
-    bool tooFarToPress, closeEnoughToPress;
+    public float maxDistance = 1.5f;
+    public float angle = 45f;
+    bool tooFarToPress, inRangeToPress;
     Material mat;
     MeshRenderer meshRenderer;
+    //By The Cookbook
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,30 +37,34 @@ public class OpenDoor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (otherTransform)
-        {
-            float dist = Vector3.Distance(otherTransform.position, transform.position);
-            if (dist <= pressableDistance)  //we are close enough 
+        float dist = Vector3.Distance(otherTransform.position, transform.position);
+        var directionToTarget = otherTransform.position - transform.position;
+        var degreesToTarget = Vector3.Angle(transform.forward, directionToTarget);
+        var withinArc = degreesToTarget < (angle / 2);
+
+            if (dist <= maxDistance)  //we are close enough 
+
             {
-                if (!closeEnoughToPress && tooFarToPress)
+                if (!inRangeToPress && tooFarToPress)
                 {
+
                     //  print("Distance to other just became Close : " + dist);
-                    closeEnoughToPress = !closeEnoughToPress;
+                    inRangeToPress = !inRangeToPress;
                     tooFarToPress = !tooFarToPress;
                     mat.color = Color.red;
+                    return;  //Added 10/20/22 seems to be ok, just to save a cycle or 2 on the CPU...
                 }
             }
-            if (dist > pressableDistance)  //we are too far   AND always true on start 
+            if (dist > maxDistance)  //we are too far   AND always true on start 
             {
-                if (closeEnoughToPress && !tooFarToPress)
+                if (inRangeToPress && !tooFarToPress)
                 {
                     //  print("Distance to other just became too Far: " + dist);
                     tooFarToPress = !tooFarToPress;
-                    closeEnoughToPress = !closeEnoughToPress;
+                    inRangeToPress = !inRangeToPress;
                     mat.color = Color.blue;
                 }
             }
-        } // if other
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -64,3 +74,34 @@ public class OpenDoor : MonoBehaviour
         anim.SetTrigger("DoorSlideDown");
     }
 }
+#if UNITY_EDITOR
+// do some custom Editor stuff 
+[CustomEditor(typeof(OpenDoor))]
+public class OpenDoorEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        var visibility = target as OpenDoor;
+        Handles.color = new Color(1, 1, 1, 0.1f);
+        var forwardPointMinusHalfAngle = Quaternion.Euler(0, -visibility.angle / 2, 0) * visibility.transform.forward;
+        Vector3 arcStart = forwardPointMinusHalfAngle * visibility.maxDistance;
+        Handles.DrawSolidArc(
+            visibility.transform.position,
+            Vector3.up,
+            arcStart,
+            visibility.angle,
+            visibility.maxDistance);
+        Handles.color = Color.white;
+        Vector3 handlePosition = visibility.transform.position +
+            visibility.transform.forward * visibility.maxDistance;
+
+        visibility.maxDistance = Handles.ScaleValueHandle(
+            visibility.maxDistance,
+            handlePosition,
+            visibility.transform.rotation,
+            1,
+            Handles.ConeHandleCap,
+            0.25f);
+    }
+}
+#endif
