040340846f355b0f84ead4c5f310c8632865dfde
BobKoto <110218823+BobKoto@users.noreply.github.com>
2022-11-18 02:24:52 -0500

    Revert "WIP Animator w/NavMesh"
    This reverts commit ad80fbc1b33640ac388775a8369fe964c4fb5a5e.

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -6,9 +6,7 @@ public class MoveTo : MonoBehaviour
 {
     Animator anim;
 
-  //  public Transform goal;
-    public Transform[] agentGoals;
-    public bool agentUpdatePosition = true; //the Default I think
+    public Transform goal;
     NavMeshAgent agent;
     enum Mode
     {
@@ -16,94 +14,44 @@ public class MoveTo : MonoBehaviour
         AnimatorControlsPosition
     }
     [SerializeField] Mode mode = Mode.AnimatorControlsPosition;
-    [SerializeField] string forwardSpeedParmeterName = "Speed";
-
-    Vector2 smoothDeltaPosition = Vector2.zero;
-    bool robotReachedDestination;
-    int agentGoalIndex = 0;
     void Start()
     {
         anim = GetComponent<Animator>();
-        anim.SetFloat(forwardSpeedParmeterName, 2f);
+        anim.SetFloat("Speed", 2f);
         agent = GetComponent<NavMeshAgent>();
-        agent.updatePosition = agentUpdatePosition;  //book recipe says set to false (so default true?)
-
-    //    agentGoals = new Transform[3];
-        agent.destination = agentGoals[0].position;
+      //  agent.
+        agent.destination = goal.position;
         var dest = agent.pathEndPosition;
-        Debug.Log("the dest is " + dest + "  The Goal is " + agentGoals[agentGoalIndex].position);
+        Debug.Log("the dest is " + dest + "  The Goal is " + goal.position);
     }
-    //private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("NavDest1"))
+        {
+            Debug.Log("Player hit NavDest1 ---set anim speed to 0 ");
+            anim.SetFloat("Speed", 0f);
+        }
+    }
+    //private void update()
     //{
-    //    if (other.gameObject.CompareTag("NavDest1"))
+    //    if (Vector3.Distance(transform.position, goal.position) < 1f)
     //    {
-    //        robotReachedDestination = true;
-    //        Debug.Log("Robot hit NavDest1 ---set anim speed to 0 ");
-    //        anim.SetFloat(forwardSpeedParmeterName, 0f);
+    //        anim.SetFloat("Speed", 0f);
     //    }
     //}
-    //private void OnTriggerExit(Collider other)
+    //private void OnAnimatorMove()
     //{
-    //    if (other.gameObject.CompareTag("NavDest1"))
+    //    switch (mode)
     //    {
-    //        robotReachedDestination = false;
-    //        Debug.Log("Robot hit NavDest1 ---set anim speed to 0 ");
-    //        anim.SetFloat(forwardSpeedParmeterName, 0f);
+    //        case Mode.AgentControlsPosition:
+    //            transform.position = agent.nextPosition;
+    //            break;
+    //        case Mode.AnimatorControlsPosition:
+    //            Vector3 position = anim.rootPosition;
+    //            position.y = agent.nextPosition.y;
+    //            transform.position = position;
+    //            break;
+
     //    }
     //}
-    private void Update()
-    {
-        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
-
-        float xMovement = Vector3.Dot(transform.right, worldDeltaPosition);
-        float zMovement = Vector3.Dot(transform.forward, worldDeltaPosition);
-
-        Vector2 localDeltaPosition = new Vector2(xMovement, zMovement);
-        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
-        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, localDeltaPosition, smooth);
-
-        var velocity = smoothDeltaPosition / Time.deltaTime;
-
-        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
-////////////////////// here test destination change /////////////
-        //if (!robotReachedDestination)
-        if (agent.remainingDistance <= agent.radius)
-        {
-            SetNextDestination();
-        }
-      
-//////////////////////end test /////////////////////////////////           
-            anim.SetFloat(forwardSpeedParmeterName, velocity.y);
-
-        if (mode == Mode.AnimatorControlsPosition)
-        {
-            if (worldDeltaPosition.magnitude > agent.radius)
-            {
-                transform.position = Vector3.Lerp(transform.position, agent.nextPosition, Time.deltaTime / 0.15f);
-            }
-        }
-    }
-    private void OnAnimatorMove()
-    {
-        switch (mode)
-        {
-            case Mode.AgentControlsPosition:
-                transform.position = agent.nextPosition;
-                break;
-            case Mode.AnimatorControlsPosition:
-                Vector3 position = anim.rootPosition;
-                position.y = agent.nextPosition.y;
-                transform.position = position;
-                break;
-
-        }
-    }
-    void SetNextDestination()
-    {
-       // Debug.Log("SetNext - agentGoalIndex is " + agentGoalIndex);
-
-        agentGoalIndex++;
-        if (agentGoalIndex >= agentGoals.Length) agentGoalIndex = 0;
-        agent.destination = agentGoals[agentGoalIndex].position;
-    }
 }
