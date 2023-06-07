0c8eb98a91164827da45d4486b6cc8a2fa4566d2
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-06-07 01:30:26 -0400

    MNT Clean a slop heap of comments

--- Assets/Scripts/ActOnMontyDoorTouch.cs
+++ Assets/Scripts/ActOnMontyDoorTouch.cs
@@ -8,44 +8,24 @@ using UnityEngine;
 public class ActOnMontyDoorTouch : MonoBehaviour, IPointerEnterHandler
 //IDragHandler, IEndDragHandler, IPointerUpHandler, IPointerExitHandler, IPointerClickHandler
 //IPointerClickHandler, IInitializePotentialDragHandler, IPointerUpHandler,IPointerExitHandler, IPointerEnterHandler, IDropHandler
-// Touch Testing only for now 
 // Component of MontySlidingDoorN objects  when N is 1,2 or 3
-// and Component of MontyPlayButton (in MontyGameIntro parent)
-// and Component of MontyGameMoveOnButton
 {
     public MontyDoorTouchEvent montyDoorTouchEvent;
     public MontyPlayButtonTouchEvent montyPlayButtonTouchEvent;
-    //public MontyMoveOnButtonTouchEvent montyMoveOnButtonTouchEvent;   //DeImp on 5/7/23
     public MontyDoorDownEvent montyDoorDownEvent;
-
     public CinemachineVirtualCamera cVCam;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       // Debug.Log("Hello from ActOnMontyDoorTouch my name is " + this.name);
-    }
     void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)  //a user touch(click)
     {
         switch (this.gameObject.name)
         {
-            case "MontyPlayButton":  //5/27/23 pretty sure no longer needed as MontyPlayButton is Deimped
-             //   Debug.Log("Object/PLAYBUTTON touched IPointerEnterHandler.OnPointerEnter " + eventData.pointerCurrentRaycast);
-                montyPlayButtonTouchEvent.Invoke();
-                break;
             case "MontySlidingDoor1":
             case "MontySlidingDoor2" :
             case "MontySlidingDoor3":
-             //   Debug.Log("Object/DOOR touched IPointerEnterHandler.OnPointerEnter " + eventData.pointerCurrentRaycast);
                 if (MontyStopTrigger.montyGameAllowDoorTouch)  //if we're NOT waiting for the 1st animation to end 
                 {
                     montyDoorTouchEvent.Invoke(IntegerToSend(this.gameObject));
                 }
                 break;
-            //case "MontyGameMoveOnButton":     //DeImp on 5/7/23
-            // //   Debug.Log("Object/MOVEonBUTTON touched IPointerEnterHandler.OnPointerEnter " + eventData.pointerCurrentRaycast);
-            //    montyMoveOnButtonTouchEvent.Invoke();
-            //    break;
             default:  
                 Debug.Log("ActOnMontyDoorTouch DEFAULTED!!!");
                 break;
@@ -70,15 +50,8 @@ public class ActOnMontyDoorTouch : MonoBehaviour, IPointerEnterHandler
     }
     public void AlertObservers(string message)
     {
-        //Debug.Log(this.name + "  received Animation event received by MontyDoorEvent... " + message);
-        //if (message.Equals("Door1DownFinished"))
-        //{
-        //    // Do other things based on an animation ending.
-        //    Debug.Log(this.name + "  received Animation event received by MontyDoorEvent... " + message);
-        //}
         int x = DoorNumberToSend(); //inspector STATIC parameters override x so BE SURE to select dynamic (in this case either could work) 
         montyDoorDownEvent.Invoke(x);   //we did try to use DoorNumberToSend() instead of x 
-        //Debug.Log("Invoked event with door # to send... " + DoorNumberToSend());
     }
     int DoorNumberToSend()
     {
@@ -97,19 +70,4 @@ public class ActOnMontyDoorTouch : MonoBehaviour, IPointerEnterHandler
         }
         return _doorNumber;
     }
-    //void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
-    //{
-    //  //  Debug.Log("Pointer ENTERED!!!! this.name = " + this.name); // + " dragging? " + eventData.dragging);
-    //}
-    //
-    //void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
-    //{
-    //   // audioManager.PlayAudio(audioManager.clipapert);
-
-    //  //  Debug.Log("Object touched IPointerClickHandler.OnPointerClick " + eventData.pointerCurrentRaycast);
-    //}
-    //void IPointerExitHandler.OnPointerExit(PointerEventData eventData)   //player took finger off cube
-    //{
-    //  //  Debug.Log("Object touched IPointerExitHandler.OnPointerExit " + eventData.pointerCurrentRaycast);
-    //}
 } // end class 
