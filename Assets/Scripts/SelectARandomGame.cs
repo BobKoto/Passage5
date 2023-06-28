eebbca10489e8cb23b0c0630b9e00bb12f61f6ef
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-06-28 02:43:48 -0400

    DEV SARGame setup randomized portals
    Created SetRandomPortals.cs to pre-randomized portals that the game will present based on how many cube rounds were won. (1,2, or 3) or if none (game over.

--- Assets/Scripts/SelectARandomGame.cs
+++ Assets/Scripts/SelectARandomGame.cs
@@ -17,11 +17,21 @@ public class SelectARandomGame: MonoBehaviour
     Vector3 centerPortal = new Vector3(203.74f, 4.82f, -217.2f);
     Vector3 rightPortal =  new Vector3(203.74f, 4.82f, -202.94f);
 
+    private SetRandomPortals randomPortalsGenerator;
+    const int numberOfPortals = 3;
+    int[] randomNumbers;
     private void Start()
     {
         audioManager = GameObject.Find("Audio Manager").GetComponent<AudioManager>();
         boxCollider = gameObject.GetComponent<BoxCollider>();
         DisableThePortals();  //6/17/23 a case for a game manager - but let's keep going because we might not go too much farther 
+
+        randomPortalsGenerator = FindObjectOfType<SetRandomPortals>();
+        randomNumbers = randomPortalsGenerator.GetRandomNumbers(numberOfPortals);
+        foreach (int number in randomNumbers)
+        {
+            Debug.Log("random portal = " + number);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -52,27 +62,78 @@ public class SelectARandomGame: MonoBehaviour
                 break;
             case 1: //Debug.Log("You won 1 cube round your only choice is to enter this world...");
                 selectRandomGameSignText.text = "1 cube round won. The only choice is to enter this portal. Walk forward to enter...";
-                teleportals[0].transform.position = centerPortal;
-                teleportals[0].SetActive(true);
+                teleportals[randomNumbers[0]].transform.position = centerPortal;
+                teleportals[randomNumbers[0]].SetActive(true);
                 break;
             case 2: //Debug.Log("You won 2 cube rounds. You can pick from these 2 worlds");
                 selectRandomGameSignText.text = "2 cube rounds won. Pick from these 2 portals. Walk into your choice...";
-                teleportals[0].transform.position = leftPortal;
-                teleportals[0].SetActive(true);
-                teleportals[1].transform.position = rightPortal;
-                teleportals[1].SetActive(true);
+                teleportals[randomNumbers[0]].transform.position = centerPortal;
+                teleportals[randomNumbers[0]].SetActive(true);
+                teleportals[randomNumbers[1]].transform.position = rightPortal;
+                teleportals[randomNumbers[1]].SetActive(true);
                 break;
             case 3: //Debug.Log("You won 3 cube rounds. You can pick from these 3 worlds");
                 selectRandomGameSignText.text = "3 cube rounds won. Pick from these 3 portals. Walk into your choice...";
-                teleportals[0].transform.position = leftPortal;
-                teleportals[0].SetActive(true);
-                teleportals[1].transform.position = centerPortal;
-                teleportals[1].SetActive(true);
-                teleportals[2].transform.position = rightPortal;
-                teleportals[2].SetActive(true);
-
+                teleportals[randomNumbers[0]].transform.position = leftPortal;
+                teleportals[randomNumbers[0]].SetActive(true);
+                teleportals[randomNumbers[1]].transform.position = centerPortal;
+                teleportals[randomNumbers[1]].SetActive(true);
+                teleportals[randomNumbers[2]].transform.position = rightPortal;
+                teleportals[randomNumbers[2]].SetActive(true);
                 break;
-
         }
     }
 }
+// following 2 classes rightoutta GPT FWIW
+//public class RandomNumberGenerator : MonoBehaviour
+//{
+//    private int[] numbers = { 1, 2, 3, 4 }; //, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+//    private void Start()
+//    {
+//        Debug.Log(" Hello from RandomNumberGenerator");
+//        int[] randomNumbers = GetRandomNumbers(3);
+
+//        foreach (int number in randomNumbers)
+//        {
+//            Debug.Log(number);
+//        }
+//    }
+
+//    public int[] GetRandomNumbers(int count)
+//    {
+//        List<int> remainingNumbers = new List<int>(numbers);
+//        List<int> randomNumbers = new List<int>();
+
+//        for (int i = 0; i < count; i++)
+//        {
+//            int index = Random.Range(0, remainingNumbers.Count);
+//            int randomNumber = remainingNumbers[index];
+//            remainingNumbers.RemoveAt(index);
+//            randomNumbers.Add(randomNumber);
+//        }
+
+//        return randomNumbers.ToArray();
+//    }
+//}
+//================== now call it ====================   moved into our original Start()
+//using UnityEngine;
+
+//public class RandomNumberCaller : MonoBehaviour
+//{
+//    private RandomNumberGenerator randomNumberGenerator;
+//    const int numberOfPortals = 3;
+//    private void Start()
+//    {
+//        randomNumberGenerator = FindObjectOfType<RandomNumberGenerator>();
+
+//        // Call the GetRandomNumbers method from the RandomNumberGenerator script
+//        int[] randomNumbers = randomNumberGenerator.GetRandomNumbers(numberOfPortals);
+
+//        foreach (int number in randomNumbers)
+//        {
+//            Debug.Log("random portal = " + number);
+//        }
+//    }
+//}
+
---
+++ Assets/Scripts/SetRandomPortals.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRandomPortals : MonoBehaviour
+{
+    private int[] numbers = { 0, 1, 2, 3 }; //, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+    public int[] GetRandomNumbers(int count)
+    {
+        List<int> remainingNumbers = new List<int>(numbers);
+        List<int> randomNumbers = new List<int>();
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = Random.Range(0, remainingNumbers.Count);
+            int randomNumber = remainingNumbers[index];
+            remainingNumbers.RemoveAt(index);
+            randomNumbers.Add(randomNumber);
+        }
+
+        return randomNumbers.ToArray();
+    }
+}
+//private void Start()
+//{
+//    Debug.Log(" Hello from RandomNumberGenerator");
+//    int[] randomNumbers = GetRandomNumbers(3);
+
+//    foreach (int number in randomNumbers)
+//    {
+//        Debug.Log(number);
+//    }
+//}
