de1b46ee7fea326184a59681e949ea178304d7d6
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-06-29 01:40:23 -0400

    DEV SARGame add random portals
    Unity changed lighting on quads attached to back of portals... Not sure we care or even need the back quads.

--- Assets/Scripts/SetRandomPortals.cs
+++ Assets/Scripts/SetRandomPortals.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SetRandomPortals : MonoBehaviour
-{
-    private int[] numbers = { 0, 1, 2, 3 }; //, 5, 6, 7, 8, 9, 10, 11, 12 };
+{//Component of SelectARandomGameTrigger
+    private int[] numbers = { 0, 1, 2, 3, 4, 5 }; //, 5, 6, 7, 8, 9, 10, 11, 12 };
 
     public int[] GetRandomNumbers(int count)
     {
