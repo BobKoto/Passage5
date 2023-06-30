52efeed4a154e89b95337083e16b4e8c1c735a40
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-06-30 01:51:55 -0400

    NO OP to flush git history on this branch

--- Assets/Scripts/SetRandomPortals.cs
+++ Assets/Scripts/SetRandomPortals.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SetRandomPortals : MonoBehaviour
-{//Component of SelectARandomGameTrigger
+{//Component of SelectARandomGameTrigger  
     [Header("Add a newly created portal(index) here.")]
     public int[] portalNumbers = { 0, 1, 2, 3, 4, 5 }; //, 5, 6, 7, 8, 9, 10, 11, 12 };
 
