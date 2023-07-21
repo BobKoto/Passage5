af12433b408cec6f6fdfd7c71b019d266e124607
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-07-21 01:58:34 -0400

    DEV Monty Game align animations
    to new position of Monty Game.
    Adjust positions of Monty box contents in the RandomTest script.

--- Assets/Scripts/RandomTest.cs
+++ Assets/Scripts/RandomTest.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using System.Security.Cryptography;
 //using Random = UnityEngine.Random;
 
 namespace randomize_array
@@ -12,6 +11,11 @@ namespace randomize_array
     {//Component of RandomTest
         int[] doors = { 1, 2, 3 };  //just to confuse me and anyone else who looks at this 
         public GameObject[] montyGameObject;
+        [Header("The position of something in the 1st opened Monty door")]
+        public Vector3 M1Position = new Vector3(204.09f, 4.51f, -214.4f); //all 3 close but refined in the editor
+        public Vector3 M2Position = new Vector3(195.51f, 4.51f, -215.75f);
+        public Vector3 M3Position = new Vector3(186.35f, 4.51f, -214.4f);
+
         public static int winningDoor;
 
         private void Start()
@@ -21,20 +25,19 @@ namespace randomize_array
             // arr[0] actually holds a 1,2 or 3 so we need to subtract 1 for the index of montyGameObject array, and so on
             // otherwise Monty Hall would ask us to pick door 0, 1, or 2... :|
 
-            Instantiate(montyGameObject[doors[0] - 1], new Vector3(237f, 3f, -219), Quaternion.Euler(0f, 271f, 0f));
+            Instantiate(montyGameObject[doors[0] - 1], M1Position, Quaternion.Euler(0f, -16.5f, 0f));  //ori V3(237f, 3f, -219)
             MeshRenderer m1 = montyGameObject[doors[0] - 1].GetComponent<MeshRenderer>();
             m1.enabled = false;
 
-            Instantiate(montyGameObject[doors[1] - 1], new Vector3(237f, 3f, -212), Quaternion.Euler(0f, 271f, 0f));
+            Instantiate(montyGameObject[doors[1] - 1], M2Position, Quaternion.Euler(0f, -16.5f, 0f));  //ori V3(237f, 3f, -212)
             MeshRenderer m2 = montyGameObject[doors[1] - 1].GetComponent<MeshRenderer>();
             m2.enabled = false;
 
-            Instantiate(montyGameObject[doors[2] - 1], new Vector3(237f, 3f, -205), Quaternion.Euler(0f, 271f, 0f));
+            Instantiate(montyGameObject[doors[2] - 1], M3Position, Quaternion.Euler(0f, -16.5f, 0f));
             MeshRenderer m3 = montyGameObject[doors[2] - 1].GetComponent<MeshRenderer>();
             m3.enabled = false;
 
             for (int i = 0; i <= doors.Length; i++)
-
             {
                 if (montyGameObject[doors[i] -1].name == "MontyGoal")
                 {
