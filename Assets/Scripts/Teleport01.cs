815d83c8371c89b62457a33188381478115ed91b
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-08-29 03:09:23 -0400

    DEV Fix teleport01.cs to also teleport non-player items
     Add tags for non player teleport-able items
    Also fix script to account for Y scale when teleporting to levels with Y positions less than zero.

--- Assets/Scripts/Teleport01.cs
+++ Assets/Scripts/Teleport01.cs
@@ -6,9 +6,9 @@ public class Teleport01 : MonoBehaviour
 {
     public Transform playerTransform; //, playerCameraRootTransform;
     public Vector3 teleportPlayerToPosition;
-   // public Quaternion rotatePlayerToRotation;
-   // public Vector3 teleportCameraRootToPosition;
-   // public Quaternion rotateCameraRootToRotation;
+    // public Quaternion rotatePlayerToRotation;
+    // public Vector3 teleportCameraRootToPosition;
+    // public Quaternion rotateCameraRootToRotation;
     //public Vector3 rotateVector;
     //public bool teleportPerformed;
     [Header("Cinemachine")]
@@ -32,51 +32,70 @@ public class Teleport01 : MonoBehaviour
             playerTransform.position = teleportPlayerToPosition; //original worked but with Jank
                                                                  //var destination = Vector3.Lerp(playerTransform.position, teleportPlayerToPosition, 0.1f);
                                                                  //playerTransform.position = destination;
-
-            Physics.SyncTransforms();
-            audioManager.PlayAudio(audioManager.teleport1);
         }
+        if (other.CompareTag("Teleportable"))
+        {
+            Transform otherTransform = other.transform;
+            if (teleportPlayerToPosition.y < 0)  //we are teleporting to a < 0 y position (lower level) so we need to account for scale
+            {
+                float yScaleOfOtherOffset = otherTransform.localScale.y /2;
+                Vector3 offsetForYScale = new Vector3(0, yScaleOfOtherOffset, 0);
+                Vector3 offsetTeleportPlayerToPosition = teleportPlayerToPosition + offsetForYScale;
+                Debug.Log("TriggerEnter ... Teleport something ELSE to Lower level" + offsetTeleportPlayerToPosition + " Other xform is " + otherTransform);
+                otherTransform.position = offsetTeleportPlayerToPosition;
 
-    }
+            }
+            else
+            {
+                Debug.Log("TriggerEnter ... Teleport something ELSE to " + teleportPlayerToPosition + " Other xform is " + otherTransform);
+                otherTransform.position = teleportPlayerToPosition;
+            }
 
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("teleport01 sees TriggerEXIT " + other.ToString());
-        //  playerTransform.SetPositionAndRotation(teleportPlayerToPosition, rotatePlayerToRotation);
-        playerTransform.rotation = Quaternion.Euler(0, 0, 0);
-        StartCoroutine(RotatePlayerCameraRoot());  //doesn't seem to work anyway
-    }
+        }
+        Physics.SyncTransforms();
+        audioManager.PlayAudio(audioManager.teleport1);
 
-    IEnumerator RotatePlayerCameraRoot()
-    {
-        Debug.Log("teleport01/RotatePlayerCameraRoot supposedly SPIN THE CAM?  BEFORE yield");
-
-        yield return new WaitForSeconds (2f);
-       
-        // playerCameraRootTransform.Rotate(rotateVector);
-        // playerCameraRootTransform.SetPositionAndRotation(playerCameraRootTransform.position, rotateCameraRootToRotation);
-       // CinemachineCameraTarget.transform.rotation = Quaternion.Euler(0f, -36f, 0f);
-           Debug.Log("teleport01/RotatePlayerCameraRoot supposedly SPIN THE CAM?  AFTER yield");
-    }
-
-    private void CameraRotation()  //Ripped right outta ThirdPersonController.cs 
-    {
-        //// if there is an input and camera position is not fixed
-        //if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
-        //{
-        //    //Don't multiply mouse input by Time.deltaTime;
-        //    float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
-
-        //    _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-        //    _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
-        //}
-
-        //// clamp our rotations so our values are limited 360 degrees
-        //_cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-        //_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
-
-        //// Cinemachine will follow this target
-        //CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
-        //    _cinemachineTargetYaw, 0.0f);
     }
 }
+
+//    private void OnTriggerExit(Collider other)
+//    {
+//        Debug.Log("teleport01 sees TriggerEXIT " + other.ToString());
+//        //  playerTransform.SetPositionAndRotation(teleportPlayerToPosition, rotatePlayerToRotation);
+//        playerTransform.rotation = Quaternion.Euler(0, 0, 0);
+//        StartCoroutine(RotatePlayerCameraRoot());  //doesn't seem to work anyway
+//    }
+
+//    IEnumerator RotatePlayerCameraRoot()
+//    {
+//        Debug.Log("teleport01/RotatePlayerCameraRoot supposedly SPIN THE CAM?  BEFORE yield");
+
+//        yield return new WaitForSeconds (2f);
+
+//        // playerCameraRootTransform.Rotate(rotateVector);
+//        // playerCameraRootTransform.SetPositionAndRotation(playerCameraRootTransform.position, rotateCameraRootToRotation);
+//       // CinemachineCameraTarget.transform.rotation = Quaternion.Euler(0f, -36f, 0f);
+//           Debug.Log("teleport01/RotatePlayerCameraRoot supposedly SPIN THE CAM?  AFTER yield");
+//    }
+
+//    private void CameraRotation()  //Ripped right outta ThirdPersonController.cs 
+//    {
+//        //// if there is an input and camera position is not fixed
+//        //if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+//        //{
+//        //    //Don't multiply mouse input by Time.deltaTime;
+//        //    float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
+//        //    _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
+//        //    _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+//        //}
+
+//        //// clamp our rotations so our values are limited 360 degrees
+//        //_cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+//        //_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+//        //// Cinemachine will follow this target
+//        //CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+//        //    _cinemachineTargetYaw, 0.0f);
+//    }
+//}
