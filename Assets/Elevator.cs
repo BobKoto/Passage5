4054ea7fa221370f1086e16bae576f58488a6a71
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-10-19 03:46:40 -0400

    DEV Avatar scene Have working Particle play ctrl
    Mod PlayParticlesOnVisibility.cs to use OnTriggerStay and Exit.
    Started review/rework of Elevator.cs to fix stuttering behavior.

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -10,79 +10,76 @@ public class Elevator : MonoBehaviour
     public float ascentIncrement = .1f;
     public float elevatorFloorPosition = .1f;
     GameObject jumpButton;
-    public bool elevatorAtTop, elevatorIsRising, elevatorIsGrounded;  //public just so we can see in editor for testing
-    bool reportOnStay = true;
+    public bool elevatorAtTop, elevatorIsRising;  //public just so we can see in editor for testing
+    bool elevatorEmpty = true;
+    bool elevatorIsGrounded = true;
     Vector3 originalPosition;
-    Coroutine raiseCoroutine, lowerCoroutine;
+    Coroutine raiseCoroutine; //, lowerCoroutine;
     // Start is called before the first frame update
     void Start()
     {
       //  Debug.Log(" Hello from Elevator ...........  ");
-        elevatorIsGrounded = true;
         jumpButton = GameObject.Find("UI_Virtual_Button_Jump");
         originalPosition = transform.position;
     }
     private void OnTriggerStay(Collider other)
     {
-        if (reportOnStay)
+        if (!other.gameObject.CompareTag("Player")) return;
+        if (elevatorEmpty)  //player has entered so disable jump button (here read as "was empty")
         {
         Debug.Log(this.name + " ENTERED by " + other.gameObject + " atTop " + elevatorAtTop + " rising " +  elevatorIsRising + " grd " + elevatorIsGrounded);
             if (jumpButton) jumpButton.SetActive(false);
+            elevatorEmpty = false;  //10/18/23 moved into this block from just below
         }
-        reportOnStay = false;
-        if (!other.gameObject.CompareTag("Player")) return;
         if (elevatorIsGrounded)
         {
             raiseCoroutine = StartCoroutine(RaiseElevator());
         }
     }
+    IEnumerator RaiseElevator()
+    {
+        elevatorIsRising = true;
+        elevatorIsGrounded = false;
+        Debug.Log("Raise elevator Coroutine Started...");
+
+        while (transform.position.y < elevatorHeight)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * ascentSpeed, Space.World); //10/18/23 changed to fixedDeltaTime  - worse stutter
+            yield return null; //10/9/23  new WaitForEndOfFrame(); // WaitForSeconds(ascentIncrement);
+            //yield return new WaitForFixedUpdate(); //10/18/23 make ascent smoother and maybe stop intermittant stutter/failure - got worse???
+
+        }
+        elevatorAtTop = true;
+        elevatorIsRising = false;
+        //elevatorIsGrounded = false;
+        Debug.Log("Raise elevator finished...");
+    }
     private void OnTriggerExit(Collider other)
     {
-        reportOnStay = true;
+        if (!other.gameObject.CompareTag("Player")) return;
+        elevatorEmpty = true;
         if (jumpButton) jumpButton.SetActive(true);
         //   Debug.Log(" Elevator EXITED...........by  " + other.gameObject);
         Debug.Log(this.name + " Elevator EXITED by " + other.gameObject + " atTop " + elevatorAtTop + " rising " + elevatorIsRising + " grd " + elevatorIsGrounded);
-        if (!other.gameObject.CompareTag("Player")) return;
+
         if (elevatorAtTop)
         {
-            lowerCoroutine = StartCoroutine(LowerElevator());
+            LowerElevator();
             return;
         }
         if (elevatorIsRising)
         {
             StopCoroutine(raiseCoroutine);
             elevatorIsRising = false;
-            lowerCoroutine = StartCoroutine(LowerElevator());
+            LowerElevator();
         }
     }
-    IEnumerator RaiseElevator()
-    {
-        Debug.Log("Raise elevator Started...");
-        elevatorIsRising = true;
-        elevatorIsGrounded = false;
-        while (transform.position.y < elevatorHeight)
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * ascentSpeed, Space.World);
-            yield return null; //10/9/23  new WaitForEndOfFrame(); // WaitForSeconds(ascentIncrement);
-        }
-        elevatorAtTop = true;
-        elevatorIsRising = false;
-        //elevatorIsGrounded = false;
-     //   Debug.Log("Raise elevator finished...");
-    }
-    IEnumerator LowerElevator()
+    void LowerElevator()
     {
         //  Debug.Log("Lower elevator Started...");
         transform.position = originalPosition;  //10/10/23 down fast and mostly unseen 
         elevatorAtTop = false;
-        //while (transform.position.y > elevatorFloorPosition)
-        //{
-        //    transform.Translate(Vector3.down * Time.deltaTime * descentSpeed, Space.World);
-        //    yield return null; //10/9/23  new WaitForEndOfFrame();  //WaitForSeconds(ascentIncrement);
-        //}
         elevatorIsGrounded = true;
-        yield return null;
-     //   Debug.Log("Lower elevator finished...");
     }
     private void OnDisable()
     {
