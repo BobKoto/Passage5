8ae07bb0fe91171b7752cf5623911d46cdf5c923
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-11-08 03:40:56 -0500

    DEV Avatar scene Work on procedural generates
    And fix LookAtObject.cs to find it's target object transform in Start()

--- Assets/RandomInstantiates.cs
+++ Assets/RandomInstantiates.cs
@@ -8,19 +8,25 @@ public class RandomInstantiates : MonoBehaviour
     public GameObject prefab;
     public Transform centerTransform;
     public int numberOfObjects = 3;
-    public float radius = 15f;
+    public float radius = 50f;
     public float yPos = 4f;
+    int randomRadius, randomRadiusX, randomRadiusZ;
     void Start()
     {
         for (int i = 0; i < numberOfObjects; i++)
         {
             float angle = i * Mathf.PI * 2 / numberOfObjects;
-            float x = Mathf.Cos(angle) * radius;
-            float z = Mathf.Sin(angle) * radius;
+            float randomLow = radius - 1;
+            float randomHigh = radius * 2;
+            randomRadiusX = (int)Random.Range(radius - 1, radius * 2);
+            randomRadiusZ = (int)Random.Range(radius - 1, radius * 2);
+            Debug.Log("random range = " + randomLow + "  " + randomHigh  +"   randomRadius = " + randomRadius);
+            float x = Mathf.Cos(angle) * randomRadiusX;
+            float z = Mathf.Sin(angle) * randomRadiusZ;
             Vector3 pos = centerTransform.position + new Vector3(x, yPos, z);
-            float angleDegrees = -angle * Mathf.Rad2Deg;
-            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
-            Instantiate(prefab, pos, rot);
+            //float angleDegrees = -angle * Mathf.Rad2Deg;
+            //Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+            Instantiate(prefab, pos, Quaternion.identity);
         }
     }
 }
--- Assets/Scripts/LookAtObject.cs
+++ Assets/Scripts/LookAtObject.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtObject : MonoBehaviour
-{
+{//Component of FloatingEye-color- Prefab(s)
     public Transform objectToLookAt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!objectToLookAt)
+        {
+            objectToLookAt = GameObject.Find("PlayerCameraRoot").GetComponent<Transform>();
+        }
     }
 
     // Update is called once per frame
